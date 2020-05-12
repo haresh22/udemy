d334c24fdd1d74921f9e9511ca2d3195befc588c
Haresh Singh <harishrsingh22@gmail.com>
2020-05-12 21:21:27 +0530

    ver 1.0

---
+++ loops_HOW_and_WHY.py
@@ -0,0 +1,11 @@
+############  loop how and why ##############
+
+# let's make a programe who can convert float number to "ROUND-UP " number
+
+# use this code for an numbers of data
+mumbai_temperture = [6.2, 7.6, 10.5, 12.3]  # Actual data
+
+for mumtemp in mumbai_temperture:  # mumtemp is dynamicaally varibale to store temp data in that
+    print(round(mumtemp))
+
+
--- user_input.py
+++ user_input.py
@@ -4,5 +4,4 @@ username = input("Enter your name :")
 location = input("Enter your location :- ")
 # This "f" string format is using only ver 3.6 or later
 user_message = f"Hi {username}! and you are located in {location}"
-
 print(user_message.title())
